3cbef5dc41cd9e2f9108feca68d870fd6f86ff1e
Licy_n <imayday5lcyz@gmail.com>
2015-02-14 09:35:57 +0800

    valentine's day

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root             'static_pages#home'
 
-  get 'help'    => 'static_pages#help'
+  get    'help'    => 'static_pages#help'
 
-  get 'about'   => 'static_pages#about'
+  get    'about'   => 'static_pages#about'
 
-  get 'contact' => 'static_pages#contact'
+  get    'contact' => 'static_pages#contact'
 
-  get 'signup'  => 'users#new'
+  get    'signup'  => 'users#new'
+
+  get    'login'   => 'sessions#new'
+
+  post   'login'   => 'sessions#create'
+
+  delete 'logout'  => 'sessions#destroy'
+  
   resources:users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
